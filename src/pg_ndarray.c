be516e9dea057b26ef0cf98f2e26d62d862a8e5e
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-01-15 16:01:53 -0600

    Initial code

--- src/pg_ndarray.c
+++ src/pg_ndarray.c
@@ -1,6 +1,6 @@
 /*
  * pg_ndarray.c:
- * 		Python / NumPy ndarray data type for Postgres
+ *     Python / NumPy ndarray data type for Postgres
  *
  * Copyright (c) 2016 Jim Nasby, Blue Treble Consulting http://BlueTreble.com
  */
@@ -10,126 +10,133 @@
 
 #include "plpython.h"
 
-static PyObject *
-PLyNdarray_FromDatum(Datum d)
+// #include "numpy/ndarrayobject.h"
+
+PG_MODULE_MAGIC;
+
+
+PG_FUNCTION_INFO_V1(PLyNdarray_FromDatum);
+Datum
+PLyNdarray_FromDatum(PG_FUNCTION_ARGS)
 {
-    text	   *txt = DatumGetByteaP(d);
-	char	   *str = VARDATA(txt);
-	size_t		size = VARSIZE(txt) - VARHDRSZ;
-    uint8_t     version;
-    static PyObject *unpickle;
-    PyObject   *pyvalue;
+  text     *txt = PG_GETARG_BYTEA_P(0);
+  char     *str = VARDATA(txt);
+  size_t    size = VARSIZE(txt) - VARHDRSZ;
+  uint8_t     version;
+  static PyObject *unpickle;
+  PyObject   *pyvalue;
 
-    /* First byte is a verision number */
-    version = *str;
-    str++;
-    size--;
+  /* First byte is a verision number */
+  version = *str;
+  str++;
+  size--;
 
-    /* Try to import cPickle.  If it doesn't exist, fall back to pickle. */
+  /* Try to import cPickle.  If it doesn't exist, fall back to pickle. */
+  if (!unpickle)
+  {
+    PyObject   *pickle_module;
+
+    pickle_module = PyImport_ImportModule("cPickle");
+    if (!pickle_module)
+    {
+      PyErr_Clear();
+      pickle_module = PyImport_ImportModule("pickle");
+    }
+    if (!pickle_module)
+      elog(ERROR, "could not import a module for unpickling");
+
+    unpickle = PyObject_GetAttrString(pickle_module, "loads");
     if (!unpickle)
-    {
-        PyObject   *pickle_module;
+      elog(ERROR, "no loads attribute in module");
+  }
 
-        pickle_module = PyImport_ImportModule("cPickle");
-        if (!pickle_module)
-        {
-            PyErr_Clear();
-            pickle_module = PyImport_ImportModule("pickle");
-        }
-        if (!pickle_module)
-            elog(ERROR, "could not import a module for unpickling");
+  switch (version)
+  {
+    case 1:
+      /*
+       * TODO: Avoid copying the string 2x 
+       * http://stackoverflow.com/questions/25067790/create-pystring-from-c-character-array-without-copying
+       */
+      pyvalue = PyObject_CallFunction(unpickle, "s", PyBytes_FromStringAndSize(str, size) );
+      if (!pyvalue)
+          elog(ERROR, "unpickling failed");
+      break;
+    default:
+      elog(ERROR, "unsupported ndarray storage version %u", version);
+  }
 
-        unpickle = PyObject_GetAttrString(pickle_module, "loads");
-        if (!unpickle)
-            elog(ERROR, "no loads attribute in module");
-    }
-
-    switch (version)
-    {
-        case 1:
-            /*
-             * TODO: Avoid copying the string 2x 
-             * http://stackoverflow.com/questions/25067790/create-pystring-from-c-character-array-without-copying
-             */
-            pyvalue = PyObject_CallFunction(unpickle, "s", PyBytes_FromStringAndSize(str, size) );
-            if (!pyvalue)
-                elog(ERROR, "unpickling failed");
-            break;
-        default:
-            elog(ERROR, "unsupported ndarray storage version %u", version);
-    }
-
-    return pyvalue;
+	return PointerGetDatum(pyvalue);
 }
 
 /*
  * Convert a numpy array to a bytea by pickling
  */
-static Datum
-PLyObject_To_ndarray(PyObject *plrv)
+PG_FUNCTION_INFO_V1(PLyObject_To_ndarray);
+Datum
+PLyObject_To_ndarray(PG_FUNCTION_ARGS)
 {
-	PyObject   *volatile plrv_so = NULL;
-    static PyObject *pickle;
-    PyObject   *pyvalue;
+  static PyObject *pickle = NULL;
+  PyObject   *volatile pyndarray = (PyObject *) PG_GETARG_POINTER(0);
+  PyObject   *pyvalue = NULL;
+  Datum       rv;
 
-	Datum		rv;
+  /*
+  if (!PyArray_Check(pyndarray))
+    ereport(ERROR,
+      (errcode(ERRCODE_WRONG_OBJECT_TYPE),
+       errmsg("not a Python numpy.ndarray")));
+       */
 
     /* Try to import cPickle.  If it doesn't exist, fall back to pickle. */
     if (!pickle)
     {
-        PyObject   *pickle_module;
+      PyObject   *pickle_module;
 
-        pickle_module = PyImport_ImportModule("cPickle");
-        if (!pickle_module)
-        {
-            PyErr_Clear();
-            pickle_module = PyImport_ImportModule("pickle");
-        }
-        if (!pickle_module)
-            elog(ERROR, "could not import a module for unpickling");
+      pickle_module = PyImport_ImportModule("cPickle");
+      if (!pickle_module)
+      {
+        PyErr_Clear();
+        pickle_module = PyImport_ImportModule("pickle");
+      }
+      if (!pickle_module)
+        elog(ERROR, "could not import a module for unpickling");
 
-        pickle = PyObject_GetAttrString(pickle_module, "dumps");
-        if (!pickle)
-            elog(ERROR, "no dumps attribute in module");
+      pickle = PyObject_GetAttrString(pickle_module, "dumps");
+      if (!pickle)
+        elog(ERROR, "no dumps attribute in module");
     }
 
 
-	Assert(plrv != Py_None);
-
-    pyvalue = PyObject_CallFunction(pickle, "O", plrv);
+    pyvalue = PyObject_CallFunction(pickle, "O", pyndarray);
 
     if (!pyvalue)
-		elog(ERROR, "could not pickle Python object");
+    elog(ERROR, "could not pickle Python object");
 
-	plrv_so = PyObject_Bytes(plrv);
-	if (!plrv_so)
-		elog(ERROR, "could not create bytes representation of Python object");
+  PG_TRY();
+  {
+    char     *plrv_sc = PyBytes_AsString(pyvalue);
+    size_t    len = PyBytes_Size(pyvalue);
+    size_t    size = len + VARHDRSZ +1;
+    bytea     *result = palloc(size);
 
-	PG_TRY();
-	{
-		char	   *plrv_sc = PyBytes_AsString(plrv_so);
-		size_t		len = PyBytes_Size(plrv_so);
-		size_t		size = len + VARHDRSZ +1;
-		bytea	   *result = palloc(size);
+    SET_VARSIZE(result, size);
 
-		SET_VARSIZE(result, size);
+    /* Store version number in first byte */
+    *VARDATA(result) = (uint8_t) 1;
 
-        /* Store version number in first byte */
-        *VARDATA(result) = (uint8_t) 1;
+    memcpy(VARDATA(result) + 1, plrv_sc, len);
+    rv = PointerGetDatum(result);
+  }
+  PG_CATCH();
+  {
+    Py_XDECREF(pyvalue);
+    PG_RE_THROW();
+  }
+  PG_END_TRY();
 
-		memcpy(VARDATA(result) + 1, plrv_sc, len);
-		rv = PointerGetDatum(result);
-	}
-	PG_CATCH();
-	{
-		Py_XDECREF(plrv_so);
-		PG_RE_THROW();
-	}
-	PG_END_TRY();
+  Py_XDECREF(pyvalue);
 
-	Py_XDECREF(plrv_so);
-
-	return rv;
+  return rv;
 }
 
 /* vi: expandtab ts=2 sw=2 */
