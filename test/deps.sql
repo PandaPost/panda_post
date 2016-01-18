0ad8c7e33d09374542c459bcd4d34e14ed14ced0
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-01-17 18:46:12 -0600

    Add plpythonu as a testdep

--- test/deps.sql
+++ test/deps.sql
@@ -1,6 +1,7 @@
 -- You will need to delete this if you do not want to use pgTap!
 CREATE SCHEMA IF NOT EXISTS tap;
-CREATE EXTENSION pgtap SCHEMA tap;
+CREATE EXTENSION IF NOT EXISTS pgtap SCHEMA tap;
 
 -- Add any test dependency statements here
+CREATE EXTENSION IF NOT EXISTS plpythonu;
 CREATE EXTENSION PandaPost;
