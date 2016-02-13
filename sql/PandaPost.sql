c020b2bc733bd5128b7b5312eb5392ff834517c9
Jim Nasby <Jim.Nasby@BlueTreble.com>
2016-02-13 15:28:17 -0600

    Whitespace

--- sql/PandaPost.sql
+++ sql/PandaPost.sql
@@ -6,9 +6,9 @@ CREATE FUNCTION ndarray_out(ndarray) RETURNS cstring LANGUAGE INTERNAL IMMUTABLE
 SET client_min_messages = NOTICE;
 
 CREATE TYPE ndarray(
-    INPUT = ndarray_in
-    , OUTPUT = ndarray_out
-    , LIKE = pg_catalog.bytea
+  INPUT = ndarray_in
+  , OUTPUT = ndarray_out
+  , LIKE = pg_catalog.bytea
 );
 
 /*
@@ -16,19 +16,18 @@ CREATE TYPE ndarray(
  * updates to it.
  */
 CREATE FUNCTION ndarray_to_plpython(internal) RETURNS internal LANGUAGE c STABLE STRICT
-    AS '$libdir/PandaPost', 'PLyNdarray_FromDatum';
+  AS '$libdir/PandaPost', 'PLyNdarray_FromDatum';
 CREATE FUNCTION ndarray_from_plpython(internal) RETURNS ndarray LANGUAGE c STABLE STRICT
-    AS '$libdir/PandaPost', 'PLyObject_To_ndarray';
+  AS '$libdir/PandaPost', 'PLyObject_To_ndarray';
 
 CREATE TRANSFORM FOR ndarray LANGUAGE plpythonu(
-    FROM SQL WITH FUNCTION ndarray_to_plpython(internal)
-    , TO SQL WITH FUNCTION ndarray_from_plpython(internal)
+  FROM SQL WITH FUNCTION ndarray_to_plpython(internal)
+  , TO SQL WITH FUNCTION ndarray_from_plpython(internal)
 );
 
 CREATE FUNCTION repr(
-    i ndarray
-) RETURNS text LANGUAGE plpythonu IMMUTABLE
-TRANSFORM FOR TYPE ndarray
+  i ndarray
+) RETURNS text LANGUAGE sql STRICT IMMUTABLE
 AS $body$
 import numpy
 
